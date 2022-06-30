ea9d546bc8fd580844e6b79e813ea2c0fb62ae52
beamlakfekad <beamlaktatek@gmail.com>
2022-06-30 14:06:03 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,3 +10,4 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
