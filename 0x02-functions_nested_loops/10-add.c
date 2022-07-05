c462b6b2a09cba731e65ce07343a21e21a749434
beamlakfekad <beamlaktatek@gmail.com>
2022-07-05 14:21:51 -0700

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - qdd 2 numners
+ * @a: integer arg
+ * @b: integer arg
+ * Return: addition of 2 numbers
+ */
+int add(int a, int b)
+{
+int sum;
+
+sum = a + b;
+return (sum);
+}
