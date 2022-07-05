3a6038e2926cc228bb39174a1a19259626d2e121
beamlakfekad <beamlaktatek@gmail.com>
2022-07-05 13:11:18 -0700

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * is_lower - checks if character is lowercase
+ * _islower - checks if character is lowercase
  * @c: character argument
  * Return: returns 1 and 0 based on condition
  */
-int is_lower(int c)
+int _islower(int c)
 {
 return (c >= 'a' && c <= 'z');
 }
