b1fb0e8abeb329426ef77b26f6b92a8e3d715254
beamlakfekad <beamlaktatek@gmail.com>
2022-07-07 12:44:45 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,13 @@ void print_diagonal(int n)
 {
 int i;
 
-if (n < 0)
+if (n <= 0)
 _putchar('\n');
 else
 {
 for (i = 0; i < n; i++)
 {
-for (j = 0; j <= i; j++)
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 _putchar(92);
