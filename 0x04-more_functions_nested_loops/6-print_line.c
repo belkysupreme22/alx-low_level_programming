ed2c36f4bd181751e2b9205aaa67a246bd84b8cf
beamlakfekad <beamlaktatek@gmail.com>
2022-07-07 11:54:08 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line- draw straight line
+ *@n: int arg
+ */
+void print_line(int n)
+{
+int i;
+
+if (n < 0)
+_putchar('\n');
+for (i = 0; i <= n; i++)
+_putchar('_');
+_putchar('\n');
+}
