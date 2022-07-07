8cc5c0a704bc457fad2c907292c1542b132f5e1b
beamlakfekad <beamlaktatek@gmail.com>
2022-07-07 11:05:13 -0700

    digit check

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: integer argument
+ * Return: 1 and 0 nased on condition
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
