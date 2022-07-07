05300e92c1ad282467c04394e8f95a2b81df8669
beamlakfekad <beamlaktatek@gmail.com>
2022-07-07 10:58:38 -0700

    uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letters
+ * @c: int parameter
+ * Return: returns 1 qnd 0 based on condition
+ */
+int _isupper(int c)
+{
+return (c >= 'A'  && c <= 'Z');
+}
